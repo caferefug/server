4278672335853b6176d205d4c039d8096bb52fb1
CreativeGP <social@cretgp.com>
2019-03-17 05:40:33 +0900

    fix

--- insert_feedback.php
+++ insert_feedback.php
@@ -6,8 +6,8 @@ if (!isset($_GET['type']) || !isset($_GET['tero_id']) || !isset($_GET['user_id']
     exit;
 }
 
+$db = getdb();
 if ($_GET['type'] == '0') {
-    $db = getdb();
     $newuser = $db->query("SELECT * FROM Ranking WHERE user_id='".$_GET['user_id']."'")->fetchAll();
     if (count($newuser) == 0) {
         $db->query("INSERT INTO Ranking (user_id, score) VALUES ('".$_GET['user_id']."', 0)");
@@ -15,7 +15,12 @@ if ($_GET['type'] == '0') {
         $db->query("UPDATE Ranking SET score = CASE WHEN score <= 0 THEN 0 ELSE score-1 END WHERE user_id='".$_GET['user_id']."'");
     }
 } else if ($_GET['type'] == '1') {
-    getdb()->query("UPDATE Ranking SET score=score+2 WHERE user_id='".$_GET['user_id']."'");
+    $newuser = $db->query("SELECT * FROM Ranking WHERE user_id='".$_GET['user_id']."'")->fetchAll();
+    if (count($newuser) == 0) {
+        $db->query("INSERT INTO Ranking (user_id, score) VALUES ('".$_GET['user_id']."', 0)");
+    } else {
+        $db->query("UPDATE Ranking SET score=score+2 WHERE user_id='".$_GET['user_id']."'");
+    }
 }
 
 if (insert_feedback($_GET['tero_id'], $_GET['user_id'], $_GET['type'])) {
