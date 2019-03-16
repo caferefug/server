45de113852ba21dac9338a01f570b3a693959257
CreativeGP <social@cretgp.com>
2019-03-17 08:59:21 +0900

    fix

--- ranking.php
+++ ranking.php
@@ -4,4 +4,12 @@ require "lib.php";
 
 $result = getdb()->query("SELECT * FROM Ranking ORDER BY score DESC")->fetchAll(/*PDO::FETCH_GROUP|PDO::FETCH_COLUMN*/);
 
+for ($result as $data) {
+    $name_json = file_get_contents("https://green.adam.ne.jp/roomazi/cgi-bin/randomname.cgi");
+    $name_json = str_replace('callback(', '', $name_json);
+    $name_json = str_replace(')', '', $name_json);
+    $data['user_name'] = json_decode($name_json, true)['name'][1];
+    //$data["user_name"] = file_get_contents("https://f25c46ee.ngrok.io/profile/" . $data["user_id"] );
+}
+
 echo json_encode($result);
