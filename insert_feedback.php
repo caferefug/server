2579d22864de94720123fc1e08800d27f4aa217b
CreativeGP <social@cretgp.com>
2019-03-17 05:38:42 +0900

    fix

--- insert_feedback.php
+++ insert_feedback.php
@@ -9,8 +9,11 @@ if (!isset($_GET['type']) || !isset($_GET['tero_id']) || !isset($_GET['user_id']
 if ($_GET['type'] == '0') {
     $db = getdb();
     $newuser = $db->query("SELECT * FROM Ranking WHERE user_id='".$_GET['user_id']."'")->fetchAll();
-    echo count($newuser);
-    $db->query("UPDATE Ranking SET score = CASE WHEN score <= 0 THEN 0 ELSE score-1 END WHERE user_id='".$_GET['user_id']."'");
+    if (count($newuser) == 0) {
+        $db->query("INSERT INTO Ranking (user_id, score) VALUES ('".$_GET['user_id']."', 0)");
+    } else {
+        $db->query("UPDATE Ranking SET score = CASE WHEN score <= 0 THEN 0 ELSE score-1 END WHERE user_id='".$_GET['user_id']."'");
+    }
 } else if ($_GET['type'] == '1') {
     getdb()->query("UPDATE Ranking SET score=score+2 WHERE user_id='".$_GET['user_id']."'");
 }
