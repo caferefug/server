240a3f63f9ea0c17849f1264b3cd3e3be9d5ddda
Yukky314159 <yuki@Yukky314159s-MacBook-Air.local>
2019-03-17 01:08:09 +0900

    Description of the changes I made

--- insert_feedback.php
+++ insert_feedback.php
@@ -2,4 +2,8 @@
 
 include "lib.php";
 
-insert_feedback($_GET['tero_id'], $_GET['user_id'], $_GET['type']);
+if (insert_feedback($_GET['tero_id'], $_GET['user_id'], $_GET['type'])) {
+	return true;
+}else{
+	return true;
+}
