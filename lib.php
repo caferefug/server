bf9451bd93aa564fc613f459c72bbe9ed8dd1abe
Yukky314159 <yuki@Yukky314159s-MacBook-Air.local>
2019-03-17 02:03:50 +0900

    Description of the changes I made

--- lib.php
+++ lib.php
@@ -1,4 +1,8 @@
 <?php
+$dsn = 'mysql:host=public.2it8h.tyo1.database-hosting.conoha.io;dbname=2it8h_development;charset=utf8';
+$db = new PDO($dsn,'2it8h_developer','Line123456789');
+$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 	function insert_feedback($tero_id = null,$user_id = null,$type = null)
 	{
@@ -6,10 +10,6 @@
                         echo "NO1";
 			return false;
 		}
-                $dsn = 'mysql:host=public.2it8h.tyo1.database-hosting.conoha.io;dbname=2it8h_development;charset=utf8';
-                $db = new PDO($dsn,'2it8h_developer','Line123456789');
-                $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 try {
         	       $sql = 'SELECT tero_id,user_id FROM Feedback WHERE tero_id=:tero_id AND user_id=:user_id';
         	       $prepare = $db->prepare($sql);
@@ -56,10 +56,32 @@
 
 	function history($tero_id,$page)
 	{
-                $dsn = 'mysql:host=public.2it8h.tyo1.database-hosting.conoha.io;dbname=2it8h_development;charset=utf8';
-                $db = new PDO($dsn,'2it8h_developer','Line123456789');
-                $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                if ($count > 1) {
+                        $start = ($count * 10) - 10;
+                }else{
+                        $start = 1;
+                }
+                try {
+                       $sql = 'SELECT img_name FROM Teros WHERE id=:tero_id limit :search,10';
+                       $prepare = $db->prepare($sql);
+                       $prepare->bindValue(':tero_id',$tero_id, PDO::PARAM_STR);
+                       $prepare->bindValue(':search',$start, PDO::PARAM_STR);
+                       $prepare->execute();
+                       $result = $prepare->fetchAll(PDO::FETCH_ASSOC);
+               } catch (PDOException $e) {
+                echo $e->getMessage();
+                return false;
+        }
+            foreach ($result as $row) {
+                $data = $row['img_name'];
+        }
+        echo json_encode($data);
+        var_dump($data);
+        return json_encode($data);
+	}
+
+        function tero_counts()
+        {
                 try {
                         $sql = 'SELECT COUNT(*) id FROM Teros';
                         $prepare = $db->prepare($sql);
@@ -67,29 +89,7 @@
                         $count = $prepare->fetchColumn();
                 } catch (PDOException $e) {
                         return false;
-                }if ($count > 1) {
-                        $start = ($count * 10) - 10;
-                }else{
-                        $start = 1;
                 }
-                try {
-                       $sql = 'SELECT id,user_id FROM Teros WHERE id=:tero_id limit :search,10';
-                       $prepare = $db->prepare($sql);
-                       $prepare->bindValue(':tero_id',$tero_id, PDO::PARAM_STR);
-                       $prepare->bindValue(':search',$start, PDO::PARAM_STR);
-                       $prepare->execute();
-                       $data = $prepare->fetchAll(PDO::FETCH_OBJ);
-               } catch (PDOException $e) {
-                echo $e->getMessage();
-                return false;
         }
-        foreach ($data as $row) {
-                $data = {$row->user_id};
-        }
-        var_dump($data);
-        $array = array('count' => $count, 'data' => $data);
-        return json_encode($array);
-	}
-
 
 
