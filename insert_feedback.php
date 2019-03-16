d9d26ef107f3a5107637026080d1328fa09b927a
CreativeGP <social@cretgp.com>
2019-03-17 05:16:14 +0900

    fix

--- insert_feedback.php
+++ insert_feedback.php
@@ -8,7 +8,8 @@ if (!isset($_GET['type']) || !isset($_GET['tero_id']) || !isset($_GET['user_id']
 
 if ($_GET['type'] == '0') {
     $db = getdb();
-    $newuser = $db->query("WHERE EXISTS(SELECT * FROM RSELECT EXISTS Ranking WHERE user_id='".$_GET['user_id']."')")->fetchAll();
+    $newuser = $db->query("SELECT * FROM WHERE user_id='".$_GET['user_id']."'")->fetchAll();
+    echo count($newuser);
     $db->query("UPDATE Ranking SET CASE WHEN score <= 0 THEN 0 ELSE score-1 END WHERE user_id='".$_GET['user_id']."'");
 } else if ($_GET['type'] == '1') {
     getdb()->query("UPDATE Ranking SET score=score+2 WHERE user_id='".$_GET['user_id']."'");
