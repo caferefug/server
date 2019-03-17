f036501b672a1932c5007eaee9838884fc91a6e9
CreativeGP <social@cretgp.com>
2019-03-17 09:02:42 +0900

    fix

--- ranking.php
+++ ranking.php
@@ -8,6 +8,7 @@ foreach ($result as &$data) {
     $name_json = file_get_contents("https://green.adam.ne.jp/roomazi/cgi-bin/randomname.cgi");
     $name_json = str_replace('callback(', '', $name_json);
     $name_json = str_replace(')', '', $name_json);
+    error_log($name_json);
     $data['user_name'] = json_decode($name_json, true)['name'][1];
     //$data["user_name"] = file_get_contents("https://f25c46ee.ngrok.io/profile/" . $data["user_id"] );
 }
