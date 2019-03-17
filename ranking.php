7b0c9091d673c4c4c6032ceb9ff64426bb41860e
Yukky314159 <yuki@Yukky314159s-MacBook-Air.local>
2019-03-17 14:11:16 +0900

    make it better

--- ranking.php
+++ ranking.php
@@ -25,3 +25,23 @@ header("X-Content-Type-Options: nosniff");
 header('Access-Control-Allow-Origin:*');
 header("Content-Type: application/json; charset=UTF-8");
 echo json_encode($result);
+
+
+/* 前のやつ。参考までに
+require "lib.php";
+
+$result = getdb()->query("SELECT * FROM Ranking ORDER BY score DESC")->fetchAll();
+
+foreach ($result as &$data) {
+    $name_json = file_get_contents("https://green.adam.ne.jp/roomazi/cgi-bin/randomname.cgi");
+    $name_json = str_replace('callback(', '', $name_json);
+    $name_json = str_replace(')', '', $name_json);
+    error_log($name_json);
+    $data['user_name'] = json_decode($name_json, true)['name'][1];
+    //$data["user_name"] = file_get_contents("https://f25c46ee.ngrok.io/profile/" . $data["user_id"] );
+}
+unset($data);
+
+echo json_encode($result);
+/*
+
