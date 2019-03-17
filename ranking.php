9725be2cb367c48ff5ef31a1f5eabb1fa5fa6ab6
CreativeGP <social@cretgp.com>
2019-03-17 09:39:46 +0900

    api fix

--- ranking.php
+++ ranking.php
@@ -9,7 +9,7 @@ foreach ($result as &$data) {
     //$name_json = str_replace('callback(', '', $name_json);
     //$name_json = str_replace(')', '', $name_json);
     //$data['user_name'] = json_decode($name_json, true)['name'][0][1];
-    $data["user_name"] = file_get_contents("https://f25c46ee.ngrok.io/profile/" . $data["user_id"] );
+    $data["user_name"] = file_get_contents("https://f25c46ee.ngrok.io/profile/" . $data["user_id"]);
 }
 unset($data);
 
