7ba7498eecb72cc6a5e320d7f2b160cf0c3b1a0c
Yukky314159 <yuki@Yukky314159s-MacBook-Air.local>
2019-03-17 14:43:13 +0900

    make it better

--- username.php
+++ username.php
@@ -3,7 +3,7 @@
 include "lib.php";
 
 if (isset($_GET['id'])) {
-	$json = what_usename($_GET['id']);
+	$json = what_username($_GET['id']);
 	header("X-Content-Type-Options: nosniff");
 	header('Access-Control-Allow-Origin:*');
 	header("Content-Type: application/json; charset=UTF-8");
