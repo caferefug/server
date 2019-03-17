196a180cc73754632a779bdea576a2e635be4768
Yukky314159 <yuki@Yukky314159s-MacBook-Air.local>
2019-03-17 11:12:49 +0900

    make it better

--- history.php
+++ history.php
@@ -7,12 +7,6 @@ if (isset($_GET['count'])) {
 	header('Access-Control-Allow-Origin:*');
 	header("Content-Type: application/json; charset=UTF-8");
 	json_encode($json);
-}elseif (isset($_GET['tero'])) {
-	$json = view_action($_GET['tero']);
-	header("X-Content-Type-Options: nosniff");
-	header('Access-Control-Allow-Origin:*');
-	header("Content-Type: application/json; charset=UTF-8");
-	echo $json;
 }else{
 $item = history($_GET['tero_id'],$_GET['page']);
 header("X-Content-Type-Options: nosniff");
